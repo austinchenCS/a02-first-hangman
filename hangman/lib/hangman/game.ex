578122eef2094509db51cb69c2373567d0f9a4dc
Austin Chen <chenas@smu.edu>
2018-09-30 17:13:44 -0500

    refactored game state structure and tally methodology

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -1,9 +1,8 @@
 defmodule GameState do
   defstruct game_state: :initializing,
             turns_left: 7,
-            word_key: [],
-            guess_state: [],
-            used: [],
+            letters: [],
+            used: ["e"],
             last_guess: ""
 end
 
@@ -13,20 +12,72 @@ defmodule Hangman.Game do
     word = Dictionary.random_word()
     |> String.codepoints()
 
-    word_redact = List.duplicate("_", length(word))
-
-    %GameState{word_key: word, guess_state: word_redact} =
-      struct(%GameState{}, word_key: word, guess_state: word_redact)
+    %GameState{letters: word} =
+      struct(%GameState{}, letters: word)
   end
 
   def tally(game) do
-    %{
-      game_state: game.game_state,
-      turns_left: game.turns_left,
-      letters: game.guess_state,
-      used: game.used
-    }
+    #IO.puts(game.letters)
+    disp_letters = build_board(game.letters, game.used)
+
+    # %{
+    #   game_state: game.game_state,
+    #   turns_left: game.turns_left,
+    #   letters: disp_letters,
+    #   used: game.used
+    # }
+
   end
+
+  # def tester([h | t], stuff) do
+  #   IO.puts(h)
+  #   IO.puts(t)
+  #   IO.puts(stuff)
+  # end
+
+  def return_letter(true, letter), do: letter
+  def return_letter(false, letter), do: "_"
+
+  def search_used(letter, used) do
+    return_letter(letter in used, letter)
+  end
+
+  def build_board([ ], used), do: []
+  def build_board([ h | t ], used) do
+    [ search_used(h, used) | build_board(t, used) ]
+  end
+
+  # def make_move(game, letter) do
+  #   # Check if letter exists within game_state
+  #   # If yes, :already_used
+  #   # If No,
+  #   #   -> Check word. If exists within word, :good_guess and word is not
+  #   # If yes,
+  #   #   -> Check word. If it does not exist within word, :bad_guess
+
+  #   already_used(game.guess_state, l)
+  #   already_used(game.guess_state, l)
+
+
+
+  # end
+
+  # defp already_used(list, letter), do: letter in list
+
+  # # function_to_call(true, ____)
+  # # function_to_call(false, ____)
+
+  # def update_state() do
+  #   is_duplicate
+
+  # end
+
+  # def is_duplicate(letter, letter),      do: :already_used
+  # def is_duplicate(letter, diff_letter), do:
+
+  # def check_guess([h | t], letter) do
+  #     [ is_duplicate(h, letter) | check_duplicate(t) ]
+  # end
 
 
 end
